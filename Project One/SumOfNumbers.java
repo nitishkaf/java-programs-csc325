4702188c9921d05b61bd13e1c162e79d2197e1a0
Nitish Kafle <47726833+kaflenitish@users.noreply.github.com>
2020-01-29 22:16:12 -0500

    Add files via upload

---
+++ Project One/SumOfNumbers.java
@@ -0,0 +1,29 @@
+package projectone;
+/*
+ * Name: Nitish Kafle
+ * Project : 1
+ * Course: CSC325
+ * Semester: Spring 2020
+ * Instructor: Nazir Nisar
+ * 
+ * This program prints the product of the numbers that are given.
+ * I used int because the program deals with integers value.
+ */
+
+
+public class SumOfNumbers {		//class name is Sum but the question is to print product
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Declaring values to the variables
+		int Num1 = 10;
+		int Num2 = 15;
+		int Num3 = 20;
+		
+		//PRINTING THE RESULT
+		System.out.println("The product of these numbers is : ");
+		System.out.println(Num1 * Num2 * Num3);	//calculation
+	}
+
+}
